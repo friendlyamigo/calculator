13e0195585848b1a255910d4b345993491924799
jackfriend <jackwillamfriend@gmail.com>
2016-01-23 17:18:49 -0500

    all buttons work

--- js/main.js
+++ js/main.js
@@ -1,41 +1,48 @@
-$(document).on('ready', function () {
+var $output = $('.field-output');
+var $input = $('.field-input');
 
-  var $output = $('.field-output');
-  var $input = $('.field-input')
 
-  function addOutput(answer) {
+
+
+
+function empty(param) {
+  if (  !(  $output.is(':empty')  )  ) {
     $output.empty();
-    $output.append(answer);
-    console.log(answer);
+    $input.val('');
   };
+  console.log('empty')
+};
+
+function addOutput(answer) {
+  $output.empty();
+  $output.append(answer);
+  console.log(answer);
+};
 
 
-  $('.btn-equals').on('click', function () {
-    var input = $input.val();
-
-    var answer = eval(input);
-
-    addOutput(answer);
-  });
 
 
-  $('.number').on('click', function (){
-    var $this = $(this)
-    var value = $this.attr('value');
-    var inputval = $input.val();
-
-    $input.val(inputval + value);
-    console.log(value);
-  });
 
 
-  $('.operation').on('click', function () {
-    var $this = $(this)
-    var value = $this.attr('value');
-    var inputval = $input.val();
+$('.number').on('click', function (){
+  empty();
 
-    $input.val(inputval + value);
-    console.log(value);
-  });
+  var $this = $(this)
+  var value = $this.attr('value');
+  var inputval = $input.val();
 
+  $input.val(inputval + value);
+  console.log(value);
+});
+
+
+$('.operation').on('click', function () {
+  empty();
+
+  var $this = $(this)
+  var value = $this.attr('value');
+  var inputval = $input.val();
+
+  $input.val(inputval + value);
+  console.log(value);
 });
